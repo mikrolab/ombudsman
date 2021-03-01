0cf26ac1b7fc9816786dcbcdd2f39459d8e22a38
GerardKil <vladhulkjoker96@gmail.com>
2021-03-01 14:06:05 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -42,23 +42,29 @@ $(document).ready(function () {
     if (validator.form()) {
       $("#form__appeal").submit(function (e) {
         e.preventDefault();
-        var form_data = {
-          'first_name': $(this).find("input[name='fsurname']").val(),
-          'middle_name': $(this).find("input[name='fname']").val(),
-          'last_name': $(this).find("input[name='fpatronymic']").val(),
-          'address': $(this).find("textarea[name='fcity']").val(),
-          'apartment_number': $(this).find("input[name='faddress']").val(),
-          'email': $(this).find("input[name='fmail']").val(),
-          'phone': $(this).find("input[name='fphone']").val(),
-          'message': $(this).find("textarea[name='fmessage']").val(),
-          'file_with_eds': $(this).find("input[name='mysign']").val(),
-          'additional_files': $(this).find("input[name='myfile']").val(),
-          'eds': $(this).find("input[name='mysignature']").val()
+        var form_data = new FormData;
+        form_data.append('first_name', $(this).find("input[name='fsurname']").val());
+        form_data.append('middle_name', $(this).find("input[name='fname']").val());
+        form_data.append('last_name', $(this).find("input[name='fpatronymic']").val());
+        form_data.append('address', $(this).find("textarea[name='fcity']").val());
+        form_data.append('apartment_number', $(this).find("input[name='faddress']").val());
+        form_data.append('email', $(this).find("input[name='fmail']").val());
+        form_data.append('phone', $(this).find("input[name='fphone']").val());
+        form_data.append('message', $(this).find("textarea[name='fmessage']").val());
+        if ($("#mysign").val() != '') {
+          form_data.append('file_with_eds', $("#mysign").prop('files')[0]);
         }
+        if ($("#myfile").val() != '') {
+          form_data.append('additional_files', $("#myfile").prop('files')[0]);
+        }
+        form_data.append('eds', $("#mysignature").prop('files')[0]);
+        console.log(form_data);
         $.ajax({
           type: "POST",
           url: "https://ombudsman-api.id-c.com.ua/api/feedback",
           data: form_data,
+          processData: false,
+          contentType: false,
           success: function () {
             $("#form__appeal").css("opacity", "0");
             setTimeout(function () {
@@ -68,14 +74,13 @@ $(document).ready(function () {
             setTimeout(function () {
               $(".form__sent").css("opacity", "1");
             }, 500)
-          }
+          },
         });
       });
     }
   });
   $('#myfile').change(function () {
     var i = $(this).prev('label').clone();
-    // var file = $('#myfile')[0].files[0].name;
     $(this).prev('label').text('Файл обрано');
   });
   $('#mysign').change(function () {
