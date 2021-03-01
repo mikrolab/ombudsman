8260639dfabd7dbba198df0afa57415ab6cc0a09
GerardKil <vladhulkjoker96@gmail.com>
2021-03-01 11:31:43 +0200

    add messege when form sent and api for form

--- src/js/main.js
+++ src/js/main.js
@@ -39,9 +39,39 @@ $(document).ready(function () {
         fmessage: "Обов’язково для заповнення"
       },
     });
-
     if (validator.form()) {
+      $("#form__appeal").submit(function (e) {
+        e.preventDefault();
+        var form_data = {
+          'first_name': $(this).find("input[name='fsurname']").val(),
+          'middle_name': $(this).find("input[name='fname']").val(),
+          'last_name': $(this).find("input[name='fpatronymic']").val(),
+          'address': $(this).find("textarea[name='fcity']").val(),
+          'apartment_number': $(this).find("input[name='faddress']").val(),
+          'email': $(this).find("input[name='fmail']").val(),
+          'phone': $(this).find("input[name='fphone']").val(),
+          'message': $(this).find("textarea[name='fmessage']").val(),
+          'file_with_eds': $(this).find("input[name='mysign']").val(),
+          'additional_files': $(this).find("input[name='myfile']").val(),
+          'eds': $(this).find("input[name='mysignature']").val()
+        }
+        $.ajax({
+          type: "POST",
+          url: "https://ombudsman-api.id-c.com.ua/api/feedback",
+          data: form_data,
+          success: function () {
+          }
+        });
+      });
       
+      $("#form__appeal").css("opacity", "0");
+      setTimeout(function(){
+        $("#form__appeal").css("display", "none");
+      }, 500)
+      $(".form__sent").css("display", "block");
+      setTimeout(function(){
+        $(".form__sent").css("opacity", "1");
+      }, 500)
     }
   });
   $('#myfile').change(function() {
